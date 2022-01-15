5ce43bec601de4593a1400334bd726a30de7cee0
Niels Peuchen <npeuchen@gmail.com>
2022-01-15 23:25:49 +0100

    Set up associations between User, Post and Comment models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
 class Comment < ApplicationRecord
+  validates :body, :post_id, :user_id, presence: true
+
+  belongs_to :post
+
+  belongs_to :user
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   validates :title, :link, :user_id, presence: true
 
   belongs_to :user
+
+  has_many :comments, dependent: :destroy
 end
